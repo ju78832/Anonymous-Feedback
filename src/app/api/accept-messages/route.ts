ddef06adfe6a609125e89df58a8d85d6ecb87479
ju78832 <junedgreat89@gmail.com>
2024-11-20 18:37:07 +0530

    Update the changes in api files

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -1,18 +1,18 @@
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/options';
-import dbConnect from '@/lib/dbConnect';
-import UserModel from '@/model/User';
-import { User } from 'next-auth';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
 
 export async function POST(request: Request) {
   // Connect to the database
   await dbConnect();
 
   const session = await getServerSession(authOptions);
-  const user: User = session?.user as User
+  const user: User = session?.user as User;
   if (!session || !session.user) {
     return Response.json(
-      { success: false, message: 'Not authenticated' },
+      { success: false, message: "Not authenticated" },
       { status: 401 }
     );
   }
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
       return Response.json(
         {
           success: false,
-          message: 'Unable to find user to update message acceptance status',
+          message: "Unable to find user to update message acceptance status",
         },
         { status: 404 }
       );
@@ -43,45 +43,40 @@ export async function POST(request: Request) {
     return Response.json(
       {
         success: true,
-        message: 'Message acceptance status updated successfully',
+        message: "Message acceptance status updated successfully",
         updatedUser,
       },
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error updating message acceptance status:', error);
+    console.error("Error updating message acceptance status:", error);
     return Response.json(
-      { success: false, message: 'Error updating message acceptance status' },
+      { success: false, message: "Error updating message acceptance status" },
       { status: 500 }
     );
   }
 }
 
-
 export async function GET(request: Request) {
-  // Connect to the database
   await dbConnect();
 
-  // Get the user session
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
-  // Check if the user is authenticated
   if (!session || !user) {
     return Response.json(
-      { success: false, message: 'Not authenticated' },
+      { success: false, message: "Not authenticated" },
       { status: 401 }
     );
   }
 
   try {
-    // Retrieve the user from the database using the ID
     const foundUser = await UserModel.findById(user._id);
 
     if (!foundUser) {
       // User not found
       return Response.json(
-        { success: false, message: 'User not found' },
+        { success: false, message: "User not found" },
         { status: 404 }
       );
     }
@@ -95,9 +90,9 @@ export async function GET(request: Request) {
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error retrieving message acceptance status:', error);
+    console.error("Error retrieving message acceptance status:", error);
     return Response.json(
-      { success: false, message: 'Error retrieving message acceptance status' },
+      { success: false, message: "Error retrieving message acceptance status" },
       { status: 500 }
     );
   }
